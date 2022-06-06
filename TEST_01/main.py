9e4ee3837918ca6d26f753edd8529e8197d2c723
yevgenyalshitz <yevgenyalshits@gmail.com>
2022-06-06 10:19:23 +0300

    update03

--- TEST_01/main.py
+++ TEST_01/main.py
@@ -3,9 +3,9 @@ import numpy as np
 import face_recognition
 
 # imgElon = face_recognition.load_image_file('Elon-Musk.jpg')
-imgElon = face_recognition.load_image_file('Images/Emma.jpg')
+imgElon = face_recognition.load_image_file('Images/Vita.jpg')
 imgElon = cv2.cvtColor(imgElon, cv2.COLOR_BGR2RGB)
-imgTest = face_recognition.load_image_file('Images/Emma2.jpg')
+imgTest = face_recognition.load_image_file('Images/Jenia2.jpg')
 # imgTest = face_recognition.load_image_file('Bill-Gates.jpg')
 imgTest = cv2.cvtColor(imgTest, cv2.COLOR_BGR2RGB)
 
