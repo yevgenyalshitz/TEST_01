c2383f3a1090aae8d3acee18cd2dac481695f5c8
yevgenyalshitz <yevgenya@emza-vs.com>
2022-11-07 15:50:34 +0200

    1st working version

--- ComputerVision/Attendance.py
+++ ComputerVision/Attendance.py
@@ -75,6 +75,10 @@ while True:
             cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
             markAttendance(name)
 
+
+    # TEMP CODE
+
+
     cv2.imshow('webcam', img)
     cv2.waitKey(1)
 
